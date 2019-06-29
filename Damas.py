63c69187d7db53e2440d86e1609311206387dd02
jon2013p <49683620+jon2013p@users.noreply.github.com>
2019-06-29 09:34:34 -0500

    Add files via upload

--- Damas.py
+++ Damas.py
@@ -1,5 +1,7 @@
 from tkinter import*
+
 tk=Tk()
+
 canvas=Canvas(tk, width=645, height=645)
 canvas.pack()
 canvas.create_rectangle(5,5,80,640)
@@ -49,49 +51,77 @@ piezza22=PhotoImage(file="piezza2.gif")
 piezza23=PhotoImage(file="piezza2.gif")
 
 
-canvas.create_image(10,6, anchor=NW, image=piezza)
-canvas.create_image(10,166, anchor=NW, image=piezza1)
-canvas.create_image(10,332, anchor=NW, image=piezza2)
-canvas.create_image(10,492, anchor=NW, image=piezza3)
-canvas.create_image(90,86, anchor=NW, image=piezza4)
-canvas.create_image(90,246, anchor=NW, image=piezza5)
-canvas.create_image(90,406, anchor=NW, image=piezza6)
-canvas.create_image(90,566, anchor=NW, image=piezza7)
-canvas.create_image(170,6, anchor=NW, image=piezza8)
-canvas.create_image(170,166, anchor=NW, image=piezza9)
-canvas.create_image(170,332, anchor=NW, image=piezza10)
-canvas.create_image(170,492, anchor=NW, image=piezza11)
+canvas.create_image(10,6, anchor=NW, image=piezza, tags="img")
+canvas.create_image(10,166, anchor=NW, image=piezza1, tags="img")
+canvas.create_image(10,332, anchor=NW, image=piezza2, tags="img")
+canvas.create_image(10,492, anchor=NW, image=piezza3, tags="img")
+canvas.create_image(90,86, anchor=NW, image=piezza4, tags="img")
+canvas.create_image(90,246, anchor=NW, image=piezza5, tags="img")
+canvas.create_image(90,406, anchor=NW, image=piezza6, tags="img")
+canvas.create_image(90,566, anchor=NW, image=piezza7, tags="img")
+canvas.create_image(170,6, anchor=NW, image=piezza8, tags="img")
+canvas.create_image(170,166, anchor=NW, image=piezza9, tags="img")
+canvas.create_image(170,332, anchor=NW, image=piezza10, tags="img")
+canvas.create_image(170,492, anchor=NW, image=piezza11, tags="img")
 
 
-canvas.create_image(410,6, anchor=NW, image=piezza12)
-canvas.create_image(410,166, anchor=NW, image=piezza13)
-canvas.create_image(410,332, anchor=NW, image=piezza14)
-canvas.create_image(410,492, anchor=NW, image=piezza15)
-canvas.create_image(490,86, anchor=NW, image=piezza16)
-canvas.create_image(490,246, anchor=NW, image=piezza17)
-canvas.create_image(490,406, anchor=NW, image=piezza18)
-canvas.create_image(490,566, anchor=NW, image=piezza19)
-canvas.create_image(570,6, anchor=NW, image=piezza20)
-canvas.create_image(570,166, anchor=NW, image=piezza21)
-canvas.create_image(570,332, anchor=NW, image=piezza22)
-canvas.create_image(570,492, anchor=NW, image=piezza23)
+canvas.create_image(410,6, anchor=NW, image=piezza12, tags="img")
+canvas.create_image(410,166, anchor=NW, image=piezza13, tags="img")
+canvas.create_image(410,332, anchor=NW, image=piezza14, tags="img")
+canvas.create_image(410,492, anchor=NW, image=piezza15, tags="img")
+canvas.create_image(490,86, anchor=NW, image=piezza16, tags="img")
+canvas.create_image(490,246, anchor=NW, image=piezza17, tags="img")
+canvas.create_image(490,406, anchor=NW, image=piezza18, tags="img")
+canvas.create_image(490,566, anchor=NW, image=piezza19, tags="img")
+canvas.create_image(570,6, anchor=NW, image=piezza20, tags="img")
+canvas.create_image(570,166, anchor=NW, image=piezza21, tags="img")
+canvas.create_image(570,332, anchor=NW, image=piezza22, tags="img")
+canvas.create_image(570,492, anchor=NW, image=piezza23, tags="img")
 
-x,y=25,15;
-def move(event):
-    global x,y
-    if (x==585 and y==15):   
-        if event.keysym=="Up":
-            canvas.move(17,0,0)
-            y=y
-        elif event.keysym=="Down":
-            canvas.move(17,0,80)
-            y=y+80
-        elif event.keysym=="Left":
-            canvas.move(17,-80,0)
-            x=x-80
-        else:
-            canvas.move(17,0,0)
-            x=x
+posicion = {"x": 0, "y": 0, "img": None}
+
+def imgPress(event):
+    posicion["img"] = canvas.find_closest(event.x, event.y)[0]
+    posicion["x"] = event.x
+    posicion["y"] = event.y
+
+def imgRelease(event):
+    posicion["img"] = None
+    posicion["x"] = 0
+    posicion["y"] = 0
+
+def imgMotion(event):
+    incremento_x = event.x - posicion["x"]
+    incremento_y = event.y - posicion["y"]
+    canvas.move(posicion["img"], incremento_x, incremento_y)
+    posicion["x"] = event.x
+    posicion["y"] = event.y
+
+canvas.tag_bind("img", "<ButtonPress-1>", imgPress)
+canvas.tag_bind("img", "<ButtonRelease-1>", imgRelease)
+canvas.tag_bind("img", "<B1-Motion>", imgMotion)
+
+
+#canvas.create_image(200, 200, anchor=tk.CENTER, image=img, tags="img")
+
+
+
+#x,y=25,15;
+#def move(event):
+#    global x,y
+#    if (x==585 and y==15):   
+#        if event.keysym=="Up":
+#            canvas.move(17,0,0)
+#            y=y
+#        elif event.keysym=="Down":
+#            canvas.move(17,0,80)
+#            y=y+80
+#        elif event.keysym=="Left":
+#            canvas.move(17,-80,0)
+#            x=x-80
+#        else:
+#            canvas.move(17,0,0)
+"""            x=x
     elif(x==585 and (y==95 or y==175 or y==255 or y==335 or y==415 or y==495)):
            
         if event.keysym=="Up":
@@ -216,6 +246,7 @@ def move(event):
 canvas.bind_all('<KeyPress-Up>', move)
 canvas.bind_all('<KeyPress-Down>', move)
 canvas.bind_all('<KeyPress-Left>', move)
-canvas.bind_all('<KeyPress-Right>', move)
+canvas.bind_all('<KeyPress-Right>', move)"""
 
 tk.mainloop()
+
